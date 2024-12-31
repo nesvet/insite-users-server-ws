28e4ac1623711e58467c8453ced423968823ceec
Eugene Nesvetaev <dev@nesvet.dev>
2024-12-31 15:46:38 +0400

    feat: Corresponding props

--- src/WSSCWithUser.ts
+++ src/WSSCWithUser.ts
@@ -1,10 +1,12 @@
 import { InSiteWebSocketServer, InSiteWebSocketServerClient } from "insite-ws/server";
 import type { AbilitiesSchema } from "insite-common";
-import type { Session, User } from "insite-users-server";
+import type { Session, SessionDoc, User } from "insite-users-server";
 
 
 export class WSSCWithUser<AS extends AbilitiesSchema> extends InSiteWebSocketServerClient {
 	declare wss: InSiteWebSocketServer<WSSCWithUser<AS>>;
+	isRejected?: boolean;
+	sessionProps?: Partial<SessionDoc>;
 	user?: User<AS>;
 	lastUserId?: string;
 	session?: Session<AS>;
